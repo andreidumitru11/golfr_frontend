8ec8f1ec80ca2b1e4d4c0faa7e91145bc00dd920
Dumitru Andrei <andrei.dumitru1121@gmail.com>
2024-07-31 13:38:16 +0300

    solve lint errors

--- pages/golfers/[id].js
+++ pages/golfers/[id].js
@@ -1,28 +1,30 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { getToken } from "../../lib/userAuth";
-import Layout from '../../components/Layout';
-import ScoreCard from '../../components/ScoreCard';
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { getToken } from '../../lib/userAuth'
+import Layout from '../../components/Layout'
+import ScoreCard from '../../components/ScoreCard'
 
 export const USER_SCORES_URL = `${process.env.NEXT_PUBLIC_API_URL}/users`
 
 const ProfilePage = () => {
-  const router = useRouter();
+  const router = useRouter()
   const { id } = router.query
 
-  const [scores, setScores] = useState([]);
-  const [error, setError] = useState('');
-  const [userName, setUserName] = useState('');
+  const [ scores, setScores ] = useState([])
+  const [ error, setError ] = useState('')
+  const [ userName, setUserName ] = useState('')
 
   useEffect(() => {
-    if(scores.length > 0)
-      setUserName(scores[0].user_name);
-  }, [scores])
+    if (scores.length > 0) {
+      setUserName(scores[0].user_name)
+    }
+  }, [ scores ])
 
   useEffect(() => {
     const fetcher = async () => {
-      if(id === null || id === undefined)
+      if (id === null || id === undefined) {
         return
+      }
       try {
         const res = await fetch(`${USER_SCORES_URL}/${id}`, {
           method: 'GET',
@@ -38,39 +40,39 @@ const ProfilePage = () => {
           throw error
         }
 
-        const data = await res.json();
-        setScores(data.scores);
-        setError('');
+        const data = await res.json()
+        setScores(data.scores)
+        setError('')
       } catch (err) {
-        setError(err);
-        setScores([]);
+        setError(err)
+        setScores([])
       }
-    };
+    }
 
-    fetcher();
-  }, [id])
+    fetcher()
+  }, [ id ])
 
   return <Layout>
-      <>
-        {error ? (
-          error
-        ) : (
-          <>
+    <>
+      {error ? (
+        error
+      ) : (
+        <>
           {userName && <h1>Scores for {userName} :</h1>}
-            {scores && scores.map(score => (
-              <ScoreCard
-                key={score.id}
-                id={score.id}
-                totalScore={score.total_score}
-                playedAt={score.played_at}
-                userId={score.user_id}
-                userName={score.user_name}
-              />
-            ))}
-          </>
-        )}
-      </>
-    </Layout>
+          {scores && scores.map(score => (
+            <ScoreCard
+              key={score.id}
+              id={score.id}
+              totalScore={score.total_score}
+              playedAt={score.played_at}
+              userId={score.user_id}
+              userName={score.user_name}
+            />
+          ))}
+        </>
+      )}
+    </>
+  </Layout>
 }
 
-export default ProfilePage;
+export default ProfilePage
