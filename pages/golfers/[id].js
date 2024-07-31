0961c9e809e075555ea40070967f15e96ebc0a34
Dumitru Andrei <andrei.dumitru1121@gmail.com>
2024-07-31 17:27:46 +0300

    create custom hook

--- pages/golfers/[id].js
+++ pages/golfers/[id].js
@@ -1,56 +1,13 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import { getToken } from '../../lib/userAuth'
 import Layout from '../../components/Layout'
 import ScoreCard from '../../components/ScoreCard'
-
-export const USER_SCORES_URL = `${process.env.NEXT_PUBLIC_API_URL}/users`
+import useUserScores from '../../lib/useUserScores'
 
 const ProfilePage = () => {
   const router = useRouter()
   const { id } = router.query
-
-  const [ scores, setScores ] = useState([])
-  const [ error, setError ] = useState('')
-  const [ userName, setUserName ] = useState('')
-
-  useEffect(() => {
-    if (scores.length > 0) {
-      setUserName(scores[0].user_name)
-    }
-  }, [ scores ])
-
-  useEffect(() => {
-    const fetcher = async () => {
-      if (id === null || id === undefined) {
-        return
-      }
-      try {
-        const res = await fetch(`${USER_SCORES_URL}/${id}`, {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${getToken()}`,
-          },
-        })
-
-        if (!res.ok) {
-          const error = new Error('An error occurred while fetching the data.')
-          error.info = await res.json()
-          error.status = res.status
-          throw error
-        }
-
-        const data = await res.json()
-        setScores(data.scores)
-        setError('')
-      } catch (err) {
-        setError(err)
-        setScores([])
-      }
-    }
-
-    fetcher()
-  }, [ id ])
+  const { scores, error, userName } = useUserScores(id)
 
   return <Layout>
     <>
